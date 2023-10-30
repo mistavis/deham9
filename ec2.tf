e96a200aeff32830e3353049f65490bda0f1e168
mistavis <joanna.e.duque@gmail.com>
2023-10-30 09:38:16 +0100

    rename instance, add local-exec

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,19 @@
 
 ### Create an EC2 instance
 
-resource "aws_instance" "joanna_webserver" {
+resource "aws_instance" "server" {
   ami                         = "ami-09100e341bda441c0"
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   key_name                    = "vockey"
-  vpc_security_group_ids      = [aws_security_group.joanna_sg_http.id]
-  subnet_id                   = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids      = [aws_security_group.allow_web_traffic.id]
+  subnet_id                   = aws_subnet.public-subnet-1.id
   tags = {
-    Name = "joanna_webserver"
+    Name = "server"
+  }
+  user_data = file("user_data.sh")
+
+  provisioner "local-exec" {
+    command = "echo Instance Type=${self.instance_type}, Instance ID=${self.id}, Public IP=${self.public_ip}, AMI ID=${self.ami} >> all_instance_details"
   }
 }
